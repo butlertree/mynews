7e0fbb90f826c8bb2fb3e4ff54f7396e031f4790
butlertree <140225441+butlertree@users.noreply.github.com>
2024-01-19 10:38:36 -0700

    Added mock data

---
+++ src/data/business.js
@@ -0,0 +1,73 @@
+const businessData = {
+    "status": "ok",
+    "totalResults": 64,
+    "articles": [
+    {
+    "source": {
+    "id": null,
+    "name": "CNBC"
+    },
+    "author": "Hakyung Kim, Samantha Subin",
+    "title": "S&P 500 falls as Treasury yields rise: Live updates - CNBC",
+    "description": "The three major averages as Treasury yields climbed higher.",
+    "url": "https://www.cnbc.com/2024/01/16/stock-market-today-live-updates.html",
+    "urlToImage": "https://image.cnbcfm.com/api/v1/image/107173255-1672767515253-gettyimages-1245963875-US_STOCKS_2023.jpeg?v=1705442968&w=1920&h=1080",
+    "publishedAt": "2024-01-17T18:24:00Z",
+    "content": "Stocks fell Wednesday as Treasury yields tracked higher following the release of stronger-than-expected U.S. economic data.\r\nThe Dow Jones Industrial Average declined 170 points, or 0.5%. The S&amp;P… [+1609 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Yahoo Entertainment"
+    },
+    "author": null,
+    "title": "Davos 2024: Wall Street weighs in on a second Donald Trump presidency - Yahoo Finance",
+    "description": null,
+    "url": "https://finance.yahoo.com/news/davos-2024-wall-street-weighs-in-on-a-second-donald-trump-presidency-174533518.html",
+    "urlToImage": null,
+    "publishedAt": "2024-01-17T17:45:33Z",
+    "content": "Si vous cliquez sur « Tout accepter », nos partenaires (y compris 244 qui font partie du Cadre de transparence et de consentement dIAB) et nous utiliserons également des témoins et vos données person… [+982 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Investor's Business Daily"
+    },
+    "author": "Investor's Business Daily",
+    "title": "Stocks Fall On China Weakness; Nvidia Drops As Cathie Wood Sells AMD Shares - Investor's Business Daily",
+    "description": null,
+    "url": "https://www.investors.com/market-trend/stock-market-today/dow-jones-falls-on-china-weakness-nvidia-tesla-fall/",
+    "urlToImage": null,
+    "publishedAt": "2024-01-17T17:29:00Z",
+    "content": null
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "YouTube"
+    },
+    "author": null,
+    "title": "Shell to Sell its Onshore Oil Drilling Business in Nigeria for $2.4 BN | Vantage with Palki Sharma - Firstpost",
+    "description": null,
+    "url": "https://www.youtube.com/watch?v=y3enB_J0WCU",
+    "urlToImage": null,
+    "publishedAt": "2024-01-17T17:25:18Z",
+    "content": "Your browser isnt supported anymore. Update it to get the best YouTube experience and our latest features. Learn more\r\nRemind me later"
+    },
+    {
+    "source": {
+    "id": "reuters",
+    "name": "Reuters"
+    },
+    "author": "Reuters",
+    "title": "Japan Airlines names former cabin attendant as first female president - Reuters",
+    "description": null,
+    "url": "https://www.reuters.com/business/aerospace-defense/japan-airlines-says-board-member-tottori-become-president-2024-01-17/",
+    "urlToImage": null,
+    "publishedAt": "2024-01-17T17:07:00Z",
+    "content": null
+    },
+]
+}
+
+export default businessData;
---
+++ src/data/health.js
@@ -0,0 +1,99 @@
+const healthData = {
+    "status": "ok",
+    "totalResults": 66,
+    "articles": [
+    {
+    "source": {
+    "id": null,
+    "name": "[Removed]"
+    },
+    "author": null,
+    "title": "[Removed]",
+    "description": "[Removed]",
+    "url": "https://removed.com",
+    "urlToImage": null,
+    "publishedAt": "1970-01-01T00:00:00Z",
+    "content": "[Removed]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "New York Post"
+    },
+    "author": "Brooke Kato",
+    "title": "Here's the reason you get sick right after being sick - New York Post ",
+    "description": "There’s a reason you’re chronically sick during flu season — COVID-19, RSV, 100-day cough, the common cold, oh my! — and, better yet, something you can do about it.",
+    "url": "https://nypost.com/2024/01/17/lifestyle/the-reason-you-get-sick-right-after-being-sick-revealed/",
+    "urlToImage": "https://nypost.com/wp-content/uploads/sites/2/2024/01/sick.gif?w=1024",
+    "publishedAt": "2024-01-17T15:34:00Z",
+    "content": "Can’t shake that wheezing cough or a bad case of the sniffles?\r\nThere’s a reason you’re chronically sick during flu season  COVID-19, RSV, 100-day cough, the common cold, oh my!  and, better yet, som… [+2588 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "BBC News"
+    },
+    "author": null,
+    "title": "Gamers at risk of irreversible hearing loss and tinnitus, study suggests - BBC.com",
+    "description": "A new review of available evidence suggests video gamers regularly exceed safe sound limits.",
+    "url": "https://www.bbc.com/news/technology-68004968",
+    "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/11674/production/_132348217_gettyimages-1222026849.jpg",
+    "publishedAt": "2024-01-17T15:10:00Z",
+    "content": "By Imran Rahman-JonesBBC News\r\nPeople who play video games are at risk of damaging their hearing due to potentially unsafe sound levels, a study has found. \r\nThe new review suggests that gamers play … [+3925 chars]"
+    },
+    {
+    "source": {
+    "id": "the-times-of-india",
+    "name": "The Times of India"
+    },
+    "author": "TOI Lifestyle Desk",
+    "title": "10 foods that fuel 'Dopamine' the happy hormone naturally - IndiaTimes",
+    "description": "Enhancing your mental well-being doesn't have to be a complicated journey. By savouring the flavors of these vegetarian foods, you not only nourish your body but also promote the production of happy hormones, paving the way for a happier and more balanced lif…",
+    "url": "https://timesofindia.indiatimes.com/life-style/health-fitness/health-news/fueling-your-happiness-with-food-that-boost-happy-hormones/photostory/106892356.cms",
+    "urlToImage": "https://static.toiimg.com/photo/106892425.cms",
+    "publishedAt": "2024-01-17T14:34:17Z",
+    "content": "Dopamine, often termed the \"feel-good\" neurotransmitter, plays a crucial role in maintaining overall well-being. It regulates mood, pleasure, and motivation, contributing to a positive mental state. … [+513 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Eatingwell.com"
+    },
+    "author": "https://www.facebook.com/EatingWell/",
+    "title": "Valerie Bertinelli's Current Favorite Snack Is Packed with 10 Grams of Protein - EatingWell",
+    "description": "In a recent post, Valerie Bertinelli shared her favorite snack that she’s been loving, and it’s a protein-packed goodie with no added sugar.",
+    "url": "https://www.eatingwell.com/valerie-bertinelli-favorite-protein-packed-snack-8430603",
+    "urlToImage": "https://www.eatingwell.com/thmb/6NfOzrlOgORfxS4eV9h8bS6KmiQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Valerie-Bertinellis-Current-Favorite-Snack-Is-Packed-with-10-Grams-of-Protein-196113a92c084d8fb1a899e7d5c69d15.jpg",
+    "publishedAt": "2024-01-17T14:21:25Z",
+    "content": "Were always tuning in to Valerie Bertinellis Instagram, where she shares insights on her health journey and her favorite healthy foods, including desserts. One of her latest Instagram posts reflects … [+2113 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "YouTube"
+    },
+    "author": null,
+    "title": "New cancer drug less toxic than chemotherapy | BBC News - BBC News",
+    "description": "Some children with cancer are receiving a new type of drug treatment far less toxic than chemotherapy.As well as being much gentler on the body, the drug is ...",
+    "url": "https://www.youtube.com/watch?v=_P_iBrFZp9o",
+    "urlToImage": "https://i.ytimg.com/vi/_P_iBrFZp9o/maxresdefault.jpg",
+    "publishedAt": "2024-01-17T14:14:59Z",
+    "content": null
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "YouTube"
+    },
+    "author": null,
+    "title": "Invasive cases of Group A strep soar to record levels - CBC News",
+    "description": "Canada is seeing a record number of cases of invasive Group A strep, a bacterial infection that kills roughly one in 10 people who contract it. Respiralogist...",
+    "url": "https://www.youtube.com/watch?v=S2Qbr2u81CQ",
+    "urlToImage": "https://i.ytimg.com/vi/S2Qbr2u81CQ/maxresdefault.jpg",
+    "publishedAt": "2024-01-17T14:07:01Z",
+    "content": null
+    },
+    ]
+}
+
+export default healthData;
