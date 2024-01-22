6640dc743b28e7b61176c21a9a8e0ca5f6a24ab5
butlertree <140225441+butlertree@users.noreply.github.com>
2024-01-22 10:00:05 -0700

    filter out articles with missing data

--- src/News/News.js
+++ src/News/News.js
@@ -5,11 +5,33 @@ import NewsImage from '../NewsImage/NewsImage'
 //PASSING THE NEWS ARRA AND THE viewCardDetails function from App.js
 function News({news, viewCardDetails}){
 
-//MAP OVER THE news ARRAY AND SET EACH NewsImage card with these key values rendering a new card component for each NewsImage
+//check if a news article has the data I need  not null and not removed
+
+function isValidArticle(article){
+
+    return (
+        
+            article.title !== null && article.title !== '[Removed]' &&
+            article.description !== null && article.description !== '[Removed]' &&
+            article.urlToImage !== null && article.urlToImage !== '[Removed]' &&
+            article.publishedAt !== null && article.publishedAt !== '[Removed]' &&
+            article.content !== null && article.content !== '[Removed]' &&
+            article.source.name !== null && article.source.name !== '[Removed]'
+        );
+
+}
+
+
+ // Filter the news array to include only valid articles
+ const validNews = news.filter(isValidArticle);
+
+
+    //MAP OVER THE (validNews) ARRAY AND SET EACH NewsImage card with these key values rendering a new card component for each NewsImage
+
 
     return (
         <div className='news-container'>
-            {news.map((article) => (
+            {validNews.map((article) => (
                 <NewsImage
                 key={article.uuid}
                 card={article} //PASSING ARTICLE OBJECT AS PROP TO NewsImage.js
